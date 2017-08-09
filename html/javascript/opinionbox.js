d3120dc7ab9ceed4fac8cd98d9090ba41a37948b
KuiHuaZi <liu758895539@qq.com>
2017-08-08 18:48:08 -0700

    [remove]some html file

--- html/javascript/opinionbox.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.star').barrating({
-        theme: 'fontawesome-stars-o',
-        showSelectedRating: true,
-        initialRating:5
-    });
-});
--- html/javascript/rotation.js
+++
@@ -1,9 +0,0 @@
-var mySwiper = new Swiper('.swiper-container',{
-    loop: true,
-    effect : 'fade',
-    autoplay: 5000,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    scrollbar: '.swiper-scrollbar',
-    pagination : '.swiper-pagination',
-});
